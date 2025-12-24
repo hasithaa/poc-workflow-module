a5f5ceff075af7f4f1ce7d8d99cfa5be602bac11
Hasitha Aravinda <hasithaa@users.noreply.github.com>
2025-12-24 09:54:20 +0530

    Add V2 code base

---
+++ build.sh
@@ -0,0 +1,27 @@
+cd native
+mvn clean package
+
+cd ../ballerina
+rm -rf  ~/.ballerina/repositories/local/bala/hasitha/workflow
+rm -rf target
+bal clean
+bal build
+bal pack
+bal push --repository=local
+
+# # If example changed or dependencies updated:
+# cd ../examples/approval
+# rm -rf target
+# bal clean
+# bal build
+
+# # Build Other Examples
+# cd ../ai_claim_approval/workflow
+# rm -rf target
+# bal clean
+# bal build
+
+# cd ../client_integration
+# rm -rf target
+# bal clean
+# bal build
